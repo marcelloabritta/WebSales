05124f5cc158cdf1a4355862b414ae763975b6da
Marcello Abritta <abrittamarcello@gmail.com>
2026-09-14 19:28:08 -0300

    Sellers Controller

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SalesWeb.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly ILogger<SellersController> _logger;
+
+        public SellersController(ILogger<SellersController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
